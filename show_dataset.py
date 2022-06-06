0c88719822e75063b2717c4b684b862784fde9dd
anras5 <anras1filip@gmail.com>
2022-06-06 18:42:44 +0200

    Add statistics

--- show_dataset.py
+++ show_dataset.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 
 ALL_COLUMN_NAMES = ['Sale', 'SalesAmountInEuro', 'time_delay_for_conversion', 'click_timestamp',
                     'nb_clicks_1week', 'product_price', 'product_age_group', 'device_type',
@@ -7,11 +7,10 @@ ALL_COLUMN_NAMES = ['Sale', 'SalesAmountInEuro', 'time_delay_for_conversion', 'c
                     'product_category(6)', 'product_category(7)', 'product_country', 'product_id',
                     'product_title', 'partner_id', 'user_id']
 
-PARTNER_ID = 'C4D189327BD87FEB3BF896DA716C6995'
+PARTNER_ID = 'BD01BAFAE73CF38C403978BBB458300C'
 
 df = pd.DataFrame()
-for chunk in pd.read_csv("../CriteoSearchData", names=ALL_COLUMN_NAMES, sep='\t', low_memory=False, chunksize=100_000):
+for chunk in pd.read_csv("../CriteoSearchData", names=ALL_COLUMN_NAMES, sep='\t', low_memory=False, chunksize=10):
     filtered_chunk = chunk[chunk['partner_id'] == PARTNER_ID]
-    df = pd.concat([df, filtered_chunk])
-
-df.to_csv(f"CriteoSearchData_{PARTNER_ID}.csv")
+    print(chunk)
+    break
